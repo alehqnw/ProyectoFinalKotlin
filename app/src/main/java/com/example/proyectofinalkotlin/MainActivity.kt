0461f57c5273c7fa67439d1c193984ea82fc5106
alehqnw <ale120970@gmail.com>
2024-01-13 23:08:53 +0000

    Commit inicial

--- app/src/main/java/com/example/proyectofinalkotlin/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalkotlin/MainActivity.kt
@@ -1,46 +1,34 @@
-package com.example.proyectofinalkotlin
+package com.example.proyectospotify
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.proyectofinalkotlin.ui.theme.ProyectoFinalKotlinTheme
+import com.example.proyectospotify.ui.navegacion.GrafoNavegacion
+import com.example.proyectospotify.ui.theme.ProyectoSPOTIFYTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProyectoFinalKotlinTheme {
+            ProyectoSPOTIFYTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GrafoNavegacion()
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoFinalKotlinTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/proyectofinalkotlin/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyectofinalkotlin/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.proyectofinalkotlin.ui.theme
+package com.example.proyectospotify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
